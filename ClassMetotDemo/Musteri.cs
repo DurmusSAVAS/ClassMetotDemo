c0e66e5cdb6a970c77f8d6df468c9da5921c84b8
durmuş savaş <durmus_savas@hotmail.com>
2021-01-24 19:26:32 +0300

    Add project files.

---
+++ ClassMetotDemo/Musteri.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class Musteri
+    {
+        public int MusteriId { get; set; }
+        public string Adınız { get; set; }
+        public string Soyadınız { get; set; }
+        public int Yaşınız { get; set; }
+       
+
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri();
+            musteri1.MusteriId = 1;
+            musteri1.Adınız = "Durmuş";
+            musteri1.Soyadınız = "SAVAŞ";
+            musteri1.Yaşınız = 29;
+            Musteri musteri2 = new Musteri();
+            musteri2.MusteriId = 2;
+            musteri2.Adınız = "Edanur";
+            musteri2.Soyadınız = "SAVAŞ";
+            musteri2.Yaşınız = 25;
+            Musteri[] urunler = new Musteri[] { musteri1, musteri2 };
+            foreach (var urun in urunler)
+            {
+                Console.WriteLine(urun.MusteriId);
+                Console.WriteLine(urun.Adınız);
+                Console.WriteLine(urun.Soyadınız);
+                Console.WriteLine(urun.Yaşınız);
+                Console.WriteLine("--------------------");
+            }
+
+
+            MusteriManager musteriManager = new MusteriManager();
+            musteriManager.Ekle(musteri1);
+
+
+            
+        }
+    }
+}
